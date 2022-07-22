fca485ad9afd5e7d2662c006fd69daa571ea6523
darentho <106845099+darentho@users.noreply.github.com>
2022-07-22 18:45:08 -0500

    I have added variables since I'll be creating an array

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+// I have stored variables with their respective values since I'll create an array with them.
+let saveBttn = $(".save-icon");
+let containerEl = $(".container");
+let schedule9am = $("#9AM");
+let schedule10am = $("#10AM");
+let schedule11am = $("#11AM");
+let schedule12pm = $("#12PM");
+let schedule1pm = $("#1PM");
+let schedule2pm = $("#2PM");
+let schedule3pm = $("#3PM");
+let schedule4pm = $("#4PM");
+let schedule5pm = $("#5PM");
